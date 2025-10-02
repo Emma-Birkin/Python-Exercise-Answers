71efe183ce185c6cb65f95e0308fbb166f3789d8
Emma Birkin <emmajbirkin@gmail.com>
2025-10-02 18:41:45 +0100

    Update Question3.py
    Added the question as a comment at the end.

--- Question3.py
+++ Question3.py
@@ -5,3 +5,4 @@ my_string_one = "Chocolate"
 my_string_two = "Cake"
 result = concat(my_string_one, my_string_two)
 print(result)
+# Write a function that takes two strings and appends the first to the second. Use the function to join the words "Chocolate" and "Cake". Print the result.
