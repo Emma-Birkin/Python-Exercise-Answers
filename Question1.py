01515e3a70c9cd160ef34981eef97646e638aaeb
Emma Birkin <emmajbirkin@gmail.com>
2025-10-02 18:40:12 +0100

    Update Question1.py
    Added the question as a comment at the end.

--- Question1.py
+++ Question1.py
@@ -4,3 +4,4 @@ def odds_print(list_of_numbers):
             print(num)
 my_list = [1,6,2,8,3,3,11]
 odds_print(my_list)
+# Write a function that prints out only the odd numbers from the following list [1,6,2,8,3,3,11] 
