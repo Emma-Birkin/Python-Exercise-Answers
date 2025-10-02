0710da16602b029f6ced9f864d42fdcf0b6b7715
Emma Birkin <emmajbirkin@gmail.com>
2025-10-02 18:44:15 +0100

    Update Question7.py
    Added the question as a comment at the end.

--- Question7.py
+++ Question7.py
@@ -4,3 +4,4 @@ def sublist(myList, start, end):
 list_of_numbers = [1,2,3,4,5,6,7,8,9]
 
 print(sublist(list_of_numbers, 2, 5))
+# Write a function that takes a list and two numbers, (see below) and returns the sublist starting from start and ending at end. def sublist(myList, start, end):
