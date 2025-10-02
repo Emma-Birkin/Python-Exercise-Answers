fdc5dbc4c2e321ec80c32f2bb3b971f00340fcb7
Emma Birkin <emmajbirkin@gmail.com>
2025-10-02 18:43:49 +0100

    Update Question6.py
    Added the question as a comment at the end.

--- Question6.py
+++ Question6.py
@@ -3,3 +3,4 @@ def returnMax(list_of_numbers):
 
 my_list = [9,4,3,2,25]
 print(returnMax(my_list))
+# Write a function that takes a list of integers and returns the maximum value contained in the list. Note, the max() function can be used. Test your function with the list [9,4,3,2,25] 
