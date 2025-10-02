92b15223595b53a6779ed184ee1de8666cee4664
Emma Birkin <emmajbirkin@gmail.com>
2025-10-02 18:42:39 +0100

    Update Question4.py
    Added the question as a comment at the end.

--- Question4.py
+++ Question4.py
@@ -1,2 +1,3 @@
 name = input("What is your name? ")
 print("Hello " + name + "!")
+# Write a function called greet that takes a string (a person's name) and directly prints a greeting in the format "Hello person's name!"
